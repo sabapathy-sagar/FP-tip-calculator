3ec157a73cbbcf391b39cacf741fca173cfbb08a
Sagar Rathna Sabapathy <33907389+sabapathy-sagar@users.noreply.github.com>
2018-11-25 11:11:59 +0100

    added complete view

--- src/View.js
+++ src/View.js
@@ -2,11 +2,40 @@ import * as R from "ramda";
 import hh from "hyperscript-helpers";
 import { h } from "virtual-dom";
 
-const { div, h1, pre } = hh(h);
+const { div, h1, pre, form, label, input } = hh(h);
 
+const formView = (dispatch, model) => {
+  //const { description, calories, showForm } = model;
+  return form(
+    {
+      className: "w-100 mv2"
+    },
+    [
+      div([
+        label({ className: "db m1" }, "Bill Amount"),
+        input({
+          className: "pa2 input-reset ba w100 mb2",
+          type: "text",
+          value: 0
+        })
+      ]),
+      div([
+        label({ className: "db m1" }, "Tip %"),
+        input({
+          className: "pa2 input-reset ba w100 mb2",
+          type: "text",
+          value: 0
+        })
+      ]),
+      div([label({ className: "db m1" }, `Tip: $${0}`)]),
+      div([label({ className: "db m1" }, `Total: $${0}`)])
+    ]
+  );
+};
 function view(dispatch, model) {
   return div({ className: "mw6 center" }, [
     h1({ className: "f2 pv2 bb" }, "Tip Calculator"),
+    formView(dispatch, model),
     pre(JSON.stringify(model, null, 2))
   ]);
 }
